b955cea88a678185487e838276371248dd845048
zzq <i@zhangzqs.cn>
2026-05-24 17:02:01 +0800

    格式化代码

--- modules/utils/mihomo_config_generator/dynamic.tf
+++ modules/utils/mihomo_config_generator/dynamic.tf
@@ -3,7 +3,10 @@ locals {
     "proxy-providers" = var.proxy_providers
     "proxy-groups"    = var.proxy_groups
     "proxies"         = var.proxies
-    "rule-providers"  = merge(local.default_rule_providers, var.extra_rule_providers)
+    "rule-providers" = merge(
+      local.default_rule_providers,
+      var.extra_rule_providers,
+    )
     "rules" = concat(
       var.extra_rules_before,
       local.default_rules,
